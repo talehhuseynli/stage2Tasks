7f1a9b95b67f97a2a3a434ce01f8bf602beb756a
talehhuseynli <taleh.hsynl@gmail.com>
2024-11-10 15:26:08 +0400

    update

---
+++ index.js
@@ -0,0 +1,58 @@
+//November 4
+//*Task-1
+// var array1 = [1, 2, 3];
+// var array2 = [2, 30, 1];
+// var result = array1.concat(array2);
+
+// console.log("result:", result);
+
+//*Task-2
+// var list = [1,2,3,[4],[5],6,[7]]
+// var result = list.flat(1)
+
+// console.log(result);
+
+//*Task-3
+// var list1 = [7, 9, 0, -2]
+// var list2 = [[1],3]
+// var result1 = list1.slice(0,1)
+// var result2 = list2.slice(0,1)
+
+// console.log(result1);
+// console.log(result2);
+
+//*Task-4
+// var list = [9,9,9,4,6,2];
+// var result = list.join("-");
+
+// console.log(result);
+
+//*Task-5/6
+// var student_list =["John","Marry","Ali","Sophia"]
+// var studentName = "John"
+// var result = (studentName + " is a student")
+
+// console.log(result);
+// console.log(studentName + " has been added to the student list.");
+
+//*Task-7
+// let array = [1, 2, 3, 4, 5];
+// let result= array.reverse();
+
+// console.log(result);
+
+//*Task-8
+// let array = [[1, 2], [3, 4], [5, 6]];
+// let result= array.flat(1);
+
+// console.log(result);
+
+//*Task-9
+// var list = ["John", "Katy", "Simpson"];
+// var lastStudent = list.length - 1;
+// var result = list[lastStudent];
+// var result2 = result + " is last student in the class";
+
+// console.log(result2);
+
+//*Task-10
