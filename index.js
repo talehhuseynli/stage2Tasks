23a40a2b83c89e41bc59f1fe25c8cb9234bbdb4f
talehhuseynli <taleh.hsynl@gmail.com>
2024-11-14 02:18:55 +0400

    update nevember 11 tasks

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-//November 4
-//*Task-1
+//? November 4
+
+//* Task-1
 // var array1 = [1, 2, 3];
 // var array2 = [2, 30, 1];
 // var result = array1.concat(array2);
 
 // console.log("result:", result);
 
-//*Task-2
+//* Task-2
 // var list = [1,2,3,[4],[5],6,[7]]
 // var result = list.flat(1)
 
 // console.log(result);
 
-//*Task-3
+//* Task-3
 // var list1 = [7, 9, 0, -2]
 // var list2 = [[1],3]
 // var result1 = list1.slice(0,1)
@@ -21,13 +22,13 @@
 // console.log(result1);
 // console.log(result2);
 
-//*Task-4
+//* Task-4
 // var list = [9,9,9,4,6,2];
 // var result = list.join("-");
 
 // console.log(result);
 
-//*Task-5/6
+//* Task-5/6
 // var student_list =["John","Marry","Ali","Sophia"]
 // var studentName = "John"
 // var result = (studentName + " is a student")
@@ -35,19 +36,19 @@
 // console.log(result);
 // console.log(studentName + " has been added to the student list.");
 
-//*Task-7
+//* Task-7
 // let array = [1, 2, 3, 4, 5];
 // let result= array.reverse();
 
 // console.log(result);
 
-//*Task-8
+//* Task-8
 // let array = [[1, 2], [3, 4], [5, 6]];
 // let result= array.flat(1);
 
 // console.log(result);
 
-//*Task-9
+//* Task-9
 // var list = ["John", "Katy", "Simpson"];
 // var lastStudent = list.length - 1;
 // var result = list[lastStudent];
@@ -55,51 +56,54 @@
 
 // console.log(result2);
 
-//*Task-10
+//* Task-10
 // var list = [1, 2, 3, 4, 5, 2];
 // var result = list.splice(1, 1);
 
 // console.log(result);
 
-//November 7
-//*Task-1
+//^ ------------------------------------------------------
+
+//? November 7
+
+//* Task-1
 // var fruit = "banana";
 // var result = fruit.length;
 
 // console.log(result);
 
-//*Task-2
+//* Task-2
 // var elementCount = 4;
 // var element = 11;
 // var result = Array(elementCount).fill(element);
 
 // console.log(result);
 
-//*Task-3
+//* Task-3
 // let text = "javascript";
 // let result = text.replace("j", "J");
 
 // console.log(result);
 
-//*Task-4
+//* Task-4
 // let fullName = "john doe";
 // let result = fullName.toUpperCase();
 
 // console.log(result);
 
-//*Task-5
+//* Task-5
 // let weather = "Today is a sunny day";
 // let result = weather.replace("sunny", "rainy");
 
 // console.log(result);
 
-//*Task-6
+//* Task-6
 // let text = "JavaScript is a funny language";
 // let result = text.split(" ");
 
 // console.log(result);
 
-//*Task-7
+//* Task-7
 //! Menim versiyam sehv oldu (Bu mene tam catmadi)
 // let fruits = ["Apple", "Banana", "Mango"];
 // let newFruits = fruits.push("Orange");
@@ -125,7 +129,7 @@
 //   console.log("axtardiginiz yoxdur");
 // }
 
-//*Task-8
+//* Task-8
 // let firstNames = ["John", "Jane"];
 // let lastNames = ["Doe", "Smith"];
 // let fullNames = firstNames.concat(lastNames);
@@ -134,7 +138,7 @@
 
 // console.log(fullNames);
 
-//*Task-9
+//* Task-9
 // let word = "JavaScript";
 // let array = word.split("");
 // result = array.reverse();
@@ -142,8 +146,57 @@
 
 // console.log(result2);
 
-//*Task-10
+//* Task-10
 // let fruits = ["Apple", "Banana", "Mango", "Orange"];
 // let result = fruits.indexOf("Banana");
 
 // console.log(result);
+
+//^ ------------------------------------------------------
+
+//? November 11
+
+//* Task-1
+//^ Dede-baba usulu:
+// let list = [1, 2, 3, 4];
+// let total = 0;
+// for (let i = 0; i < list.length; i++) {
+//   total += list[i];
+// }
+// console.log("Task-1",total);
+
+//^ For of usulu:
+// let list = [1, 2, 3, 4];
+// let total = 0;
+// for (let el of list) {
+//   total += el;
+// }
+// console.log("Task-1", total);
+
+//* Task-2
+// let list = [1, 2, 3, 4, 5];
+// let total = [1];
+// for (let el of list) {
+//   total *= el;
+// }
+// console.log("Task-2",total);
+
+//* Task-3
+// let list = [3, 7, 2, 8, 5];
+// let findMax = -Infinity;
+// for (let i = 0; i < list.length; i++) {
+//   let el = list[i];
+//   if (findMax < el) {
+//     findMax = el;
+//   }
+// }
+// console.log("Task-3", findMax);
+
+//* Task-4
+// let a = "hello";
+// let b = a.split("");
+// let c = b.reverse();
+// let d = c.join("");
+// console.log(d);
+
+//* Task-5
